c51313aefbdc1594a993b2f67d306ad4565add09
Egor <65542192+Sillor@users.noreply.github.com>
2023-12-22 16:50:49 -0800

    Add endpoints for getting and updating user data

--- server.js
+++ server.js
@@ -25,14 +25,19 @@ const handleDatabaseError = (res, err) => {
 app.post('/api/signup', async (req, res) => {
     const { username, password } = req.body;
 
-    if (!username || !password || password.length < 6 || username.length < 4) {
+    if (!username || !password || password.length < 6 || username.length < 4)
         return res.json({ status: 'error', error: 'Invalid username/password' });
-    }
 
     try {
+        const [[existingUser]] = await db.query('SELECT * FROM credentials WHERE username = ?', [username]);
+        if (existingUser) {
+            return res.json({ status: 'error', error: 'Username already exists' });
+        }
+
+        const [{ insertId }] = await db.query('INSERT INTO users (taps, timeSpent) VALUES (0, 0)');
         await db.query(
-            'INSERT INTO user_credentials (username, password, date_created) VALUES (?, ?, ?)',
-            [username, await bcrypt.hash(password, 10), new Date()]
+            'INSERT INTO credentials (username, password, date_created, userId) VALUES (?, ?, ?, ?)',
+            [username, await bcrypt.hash(password, 10), new Date(), insertId]
         );
         res.json({ status: 'success', message: 'User created' });
     } catch (err) {
@@ -40,43 +45,97 @@ app.post('/api/signup', async (req, res) => {
     }
 });
 
-app.post('/api/login', async (req, res) => {
-    const { username, password } = req.body;
-
+app.put('/api/user', authenticateToken, async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM user_credentials WHERE username = ?', [username]);
-        const isValidPassword = results[0] && await bcrypt.compare(password, results[0].password);
+        const { taps, timeSpent } = req.body;
+        await db.query('UPDATE users SET taps = ?, timeSpent = ? WHERE id = ?', [taps, timeSpent, req.user.userId]);
+        res.json({ status: 'success', message: 'User updated' });
+    } catch (err) {
+        handleDatabaseError(res, err);
+    }
+});
 
-        const accessToken = generateAccessToken({ username });
-        const refreshToken = jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET);
+app.put('/api/user/upgrades', authenticateToken, async (req, res) => {
+    try {
+        for (const [name, amount] of Object.entries(req.body)) {
+            await db.query(
+                'INSERT INTO upgrades (userId, upgradeName, upgradeValue) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE upgradeValue = ?',
+                [req.user.userId, name, amount, amount]
+            );
+        }
+        res.json({ status: 'success', message: 'User upgrades updated' });
+    } catch (err) {
+        handleDatabaseError(res, err);
+    }
+});
 
-        res.json({
-            status: isValidPassword ? 'success' : 'error',
-            message: isValidPassword ? 'Login successful' : 'Invalid username/password',
-            accessToken: isValidPassword ? accessToken : null,
+app.put('/api/user/stats', authenticateToken, async (req, res) => {
+    try {
+        const { buttonClicked, upgradesPurchased, maxCps } = req.body;
+        await db.query('INSERT INTO stats (userId, buttonClicked, upgradesPurchased, maxCps) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE buttonClicked = ?, upgradesPurchased = ?, maxCps = ?', [req.user.userId, buttonClicked, upgradesPurchased, maxCps, buttonClicked, upgradesPurchased, maxCps]);
+        res.json({ status: 'success', message: 'User updated' });
+    } catch (err) {
+        handleDatabaseError(res, err);
+    };
+});
+
+app.post('/api/login', async (req, res) => {
+    try {
+        const [[user]] = await db.query('SELECT * FROM credentials WHERE username = ?', [req.body.username]);
+        const isValid = user && await bcrypt.compare(req.body.password, user.password);
+
+        res.json(isValid ? {
+            status: 'success',
+            message: 'Login successful',
+            accessToken: generateAccessToken({ userId: user.userId }),
+        } : {
+            status: 'error',
+            message: 'Invalid username/password',
         });
     } catch (err) {
         handleDatabaseError(res, err);
     }
 });
 
-app.get('/api/users', authenticateToken, async (req, res) => {
+app.get('/api/user', authenticateToken, async (req, res) => {
     try {
-        const [results] = await db.query('SELECT * FROM user_credentials WHERE username = ?', [req.user.username]);
-        res.json({ status: 'success', users: results });
+        const [[results]] = await db.query('SELECT * FROM users WHERE id = ?', [req.user.userId]);
+        res.json({ status: 'success', data: results });
     } catch (err) {
         handleDatabaseError(res, err);
     }
 });
+
+app.get('/api/user/upgrades', authenticateToken, async (req, res) => {
+    try {
+        const [results] = await db.query('SELECT * FROM upgrades WHERE userId = ?', [req.user.userId]);
+        const upgrades = results.reduce((acc, upgrade) => {
+            acc[upgrade.upgradeName] = upgrade.upgradeValue;
+            return acc;
+        }, {});
+        res.json({ status: 'success', data: upgrades });
+    } catch (err) {
+        handleDatabaseError(res, err);
+    }
+});
+
+app.get('/api/user/stats', authenticateToken, async (req, res) => {
+    try {
+        const [results] = await db.query('SELECT buttonClicked, upgradesPurchased, maxCps FROM stats WHERE userId = ?', [req.user.userId]);
+        res.json({ status: 'success', data: results[0] });
+    } catch (err) {
+        handleDatabaseError(res, err);
+    }
+}
+);
 
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5m' });
 }
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
